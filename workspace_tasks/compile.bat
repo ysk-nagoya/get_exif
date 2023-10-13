e0d773ff5100b37e3eb55f507afaec9277234deb
ysk-nagoya <ysknagoya816@gmail.com>
2023-10-14 04:11:31 +0900

    Change: compile command

--- workspace_tasks/compile.bat
+++ workspace_tasks/compile.bat
@@ -4,4 +4,4 @@ set python_file=D:\my_repositories\get_exif\get_exif\get_exif.py
 set output_dir=D:\my_repositories\get_exif\compile\get_exif_compile
 
 call activate get-exif
-nuitka %python_file% --onefile --remove-output --follow-imports --enable-plugin=tk-inter --output-dir=%output_dir%
+nuitka %python_file% --onefile --remove-output --show-modules --mingw64 --follow-imports --enable-plugin=tk-inter --output-dir=%output_dir%

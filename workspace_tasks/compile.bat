bb8a34639205f7510771c678a41abff35b55eed6
ysk-nagoya <ysknagoya816@gmail.com>
2023-10-14 01:44:09 +0900

    Change: compile bat

--- workspace_tasks/compile.bat
+++ workspace_tasks/compile.bat
@@ -1,10 +1,7 @@
 @echo off
 
-set output_dir=%1\compile\sg-ami_process
-set python_file=%1\get_exif\get_exif.py
+set output_dir=D:\my_repositories\get_exif\compile\get_exif_compile
+set python_file=D:\my_repositories\get_exif\get_exif\get_exif.py
 
-call activate ""
-nuitka %python_file% --standalone --remove-output --show-modules --mingw64 --follow-imports --plugin-enable=pyside6 --include-qt-plugins=platforms --output-dir=%output_dir%
-
-@REM nuitka=1.5.0
-@REM mingw64=v11.3.0
+call activate get-exif
+nuitka %python_file% --standalone --remove-output --follow-imports --enable-plugin=tk-inter --enable-plugin=matplotlib --output-dir=%output_dir%
